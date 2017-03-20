7afb6a78fedc2e9d49cc2262f8ced09e50c8af8a
mawasi <mawasi99@hotmail.com>
2017-03-21 03:42:11 +0900

    PlatformsdkをMarshmallowに固定。 MobileDataオン、オフ入れようとした残骸追加。あとで消す。 テザリング実装用のtodo追加。

--- MainActivity.cs
+++ MainActivity.cs
@@ -20,7 +20,7 @@ namespace NetworkDeviceSwitch
 		// NetworkStateReceiverのマニフェスト登録用文字列
 
 		// ネットワーク切り替え時にブロードキャストされる
-		public const string CONNECTIVITY_CHANGE = "android.net.conn.CONNECTIVITY_CHANGE";
+		public const string CONNECTIVITY_CHANGE = ConnectivityManager.ConnectivityAction;
 
 		public const string PhoneStateChanged = TelephonyManager.ActionPhoneStateChanged;
 
@@ -138,9 +138,16 @@ namespace NetworkDeviceSwitch
 		/// <param name="e"></param>
 		void OnTetheringSwitchCheckedChange(object sender, CompoundButton.CheckedChangeEventArgs e)
 		{
-
+			// ConnectivityManager, WifiManagerあたりで何か探してみる
+			// Wifi-Access Pointとかで調べる
+			// https://github.com/xamarin/xamarin-android githubのこれ見てみる
 		}
 
+		/// <summary>
+		/// Toggle Wifi switch
+		/// </summary>
+		/// <param name="enabled"></param>
+		/// <returns></returns>
 		bool ToggleWifi(bool enabled)
 		{
 			bool result = false;
@@ -152,7 +159,11 @@ namespace NetworkDeviceSwitch
 			return result;
 		}
 
-
+		/// <summary>
+		/// Toggle mobile data switch
+		/// </summary>
+		/// <param name="enabled"></param>
+		/// <returns></returns>
 		bool ToggleMobileData(bool enabled)
 		{
 			bool result = false;
@@ -161,21 +172,22 @@ namespace NetworkDeviceSwitch
 				if(Build.VERSION.SdkInt >= BuildVersionCodes.Lollipop) {
 			//		settings
 					// クラスインスタンス取得
-					Java.Lang.Class telephonManClass = Java.Lang.Class.ForName(mTelephonyManager.Class.Name);
-					Java.Lang.Reflect.Field[] fields = telephonManClass.GetDeclaredFields();
-					if(enabled == true) {
-						Console.WriteLine("##### Start Field List #####");
-						foreach(var field in fields) {
-							Console.WriteLine(field.Name);
-						}
-						Console.WriteLine("##### End Field List #####");
-						Java.Lang.Reflect.Method[] methods = telephonManClass.GetDeclaredMethods();
-						Console.WriteLine("##### Start Methd List #####");
-						foreach(var method in methods) {
-							Console.WriteLine(method.Name);
-						}
-						Console.WriteLine("##### End Methd List #####");
-					}
+//					Java.Lang.Class telephonManClass = Java.Lang.Class.ForName(mTelephonyManager.Class.Name);
+//					Java.Lang.Reflect.Field[] fields = telephonManClass.GetDeclaredFields();
+//					if(enabled == true) {
+//						Console.WriteLine("##### Start Field List #####");
+//						foreach(var field in fields) {
+//							Console.WriteLine(field.Name);
+//						}
+//						Console.WriteLine("##### End Field List #####");
+//						Java.Lang.Reflect.Method[] methods = telephonManClass.GetDeclaredMethods();
+//						Console.WriteLine("##### Start Methd List #####");
+//						foreach(var method in methods) {
+//							Console.WriteLine(method.Name);
+//						}
+//						Console.WriteLine("##### End Methd List #####");
+//					}
+					ToggleMobileDatafromL(enabled);
 				}	// Gingerbread以上 KitkatWatch以下の実装
 				if(Build.VERSION.SdkInt <= BuildVersionCodes.KitkatWatch
 						&& Build.VERSION.SdkInt >= BuildVersionCodes.Gingerbread) {
@@ -193,6 +205,60 @@ namespace NetworkDeviceSwitch
 			}
 
 			return result;
+		}
+
+		/// <summary>
+		/// Toggle mobiledata enabled.
+		/// From Lollipop.
+		/// Reference http://stackoverflow.com/questions/26539445/the-setmobiledataenabled-method-is-no-longer-callable-as-of-android-l-and-later
+		/// </summary>
+		/// <param name="enabled"></param>
+		/// <returns></returns>
+		bool ToggleMobileDatafromL(bool enabled)
+		{
+			bool result = false;
+
+			int state = 0;
+			try {
+
+				state = IsMobileDataEnabledFromLollipos() ? 0 : 1;
+
+				if(state == 1) {
+					Console.WriteLine("Mobile Data Enabled. from lollipop");
+				}
+				else {
+					Console.WriteLine("Mobile Data Disabled. from lollipos");
+				}
+
+				// Lollipopよりバージョンが上(API 22以上)かどうかで更に挙動が変わる
+				if(Build.VERSION.SdkInt > BuildVersionCodes.Lollipop) {
+				
+				}
+				else if(Build.VERSION.SdkInt == BuildVersionCodes.Lollipop) { // Lollipop(API 21)
+
+				}
+
+			}
+			catch(Exception e) {
+				
+			}
+
+			return result;
+		}
+
+		/// <summary>
+		/// Mobile data enabled check
+		/// </summary>
+		/// <param name="context"></param>
+		/// <returns>mobile data is disable. return true</returns>
+		bool IsMobileDataEnabledFromLollipos()
+		{
+			bool state = false;
+			if(Build.VERSION.SdkInt >= BuildVersionCodes.Lollipop) {
+				state = Android.Provider.Settings.Global.GetInt(ContentResolver, "mobile_data", 0) == 1;
+			}
+
+			return state;
 		}
 
 		/// <summary>
@@ -218,7 +284,7 @@ namespace NetworkDeviceSwitch
 				getMobileDataEnableMethod.Accessible = true;
 
 				// 現在の状態と違ってたら設定する
-				// Lollipop以降は Java.Lang.NoSuchMethodException: で怒られることを確認
+				// Lollipop以降は Java.Lang.NoSuchMethodException の例外が発生することを確認
 				if(enabled != (bool)getMobileDataEnableMethod.Invoke(iConnectivityManager)) {
 					setMobileDataEnableMethod.Invoke(iConnectivityManager, enabled);
 					result = true;
